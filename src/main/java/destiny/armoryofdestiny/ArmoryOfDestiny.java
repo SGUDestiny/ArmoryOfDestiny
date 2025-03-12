c730f3b381d53b8f10c4fceabec3a11dd9946071
SGUDestiny <panormov2006@gmail.com>
2025-03-13 00:50:20 +0400

    Added mixin refmap to fix compat issues

--- src/main/java/destiny/armoryofdestiny/ArmoryOfDestiny.java
+++ src/main/java/destiny/armoryofdestiny/ArmoryOfDestiny.java
@@ -1,12 +1,7 @@
 package destiny.armoryofdestiny;
 
 import com.mojang.logging.LogUtils;
-import destiny.armoryofdestiny.client.render.item.MetallicFeatherRenderer;
-import destiny.armoryofdestiny.client.render.entity.PelletRenderer;
-import destiny.armoryofdestiny.client.render.entity.BuckshotRenderer;
-import destiny.armoryofdestiny.client.render.entity.ExplosiveSlugRenderer;
-import destiny.armoryofdestiny.client.render.entity.SlugRenderer;
-import destiny.armoryofdestiny.client.render.entity.SparkRenderer;
+import destiny.armoryofdestiny.client.render.entity.MetallicFeatherRenderer;
 import destiny.armoryofdestiny.server.event.CommonEvents;
 import destiny.armoryofdestiny.server.item.utility.*;
 import destiny.armoryofdestiny.server.registry.*;
@@ -14,19 +9,15 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 @Mod(ArmoryOfDestiny.MODID)
